8c79ade88f6ee054264c8851d218b5f3ddf797a1
ckhowell <ckhowell@me.com>
2026-02-16 18:15:53 +1000

    SEO and Podcast

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jasonhowes.com.au',
+  integrations: [sitemap()],
   devToolbar: { enabled: false },
   vite: {
     plugins: [tailwindcss()]
